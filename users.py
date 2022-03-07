a081c8574ee5b098b7e86a81be7fcd4a4315a4a1
Christophe HABIMANA <88231007+KhrisTheLearner@users.noreply.github.com>
2022-03-07 17:23:56 +0200

    Add files via upload

---
+++ users.py
@@ -0,0 +1,89 @@
+user1 = {
+    "username":"khristof",
+    "PIN":1234,
+    "balance":{
+        "Ksh":140,
+        "USD":0
+    }
+
+}
+
+user2 = {
+    "username":"kate",
+    "PIN":5678,
+    "balance":{
+        "Ksh":150,
+        "USD":5
+    }
+
+}
+
+user3 = {
+    "username":"Alex",
+    "PIN":9012,
+    "balance":{
+        "Ksh":155,
+        "USD":10
+    }
+
+}
+
+user4 = {
+    "username":"Lynette",
+    "PIN":3456,
+    "balance":{
+        "Ksh":160,
+        "USD":15
+    }
+
+}
+
+user5 = {
+    "username":"Joseph",
+    "PIN":7890,
+    "balance":{
+        "Ksh":165,
+        "USD":20
+    }
+
+}
+
+user6 = {
+    "username":"Aketch",
+    "PIN":1123,
+    "balance":{
+        "Ksh":170,
+        "USD":25
+    }
+
+}
+
+user7 = {
+    "username":"Wabuko",
+    "PIN":1456,
+    "balance":{
+        "Ksh":175,
+        "USD":30
+    }
+
+}
+
+user8 = {
+    "username":"Becky",
+    "PIN":1567,
+    "balance":{
+        "Ksh":180,
+        "USD":35
+    }
+
+}
+
+user9 = {
+    "username":"oliver",
+    "PIN":1678,
+    "balance":{
+        "Ksh":185,
+        "USD":40
+    }
+
+}
